56f894ee8c3e7387166c019b1aa0c195ba101fa1
LuisCunha05 <luisf.cunha05@gmail.com>
2024-08-27 11:03:55 -0400

    added home page
    changed deleteChildren() to use pack_forget instead of destroy

--- voucher/oferta2/avaliacao/restaurante.py
+++ voucher/oferta2/avaliacao/restaurante.py
@@ -39,9 +39,9 @@ def addEntry(origin: tk.Frame, e_kwargs: dict = {}, p_kwargs: dict = {}) -> tk.E
     entry.pack(**p_kwargs)
     return entry
 
-def deleteChildren(object: tk.Tk | tk.Frame):
+def forgetChildren(object: tk.Tk | tk.Frame):
     for child in object.winfo_children():
-        child.destroy()
+        child.pack_forget()
 
 
 
@@ -68,6 +68,10 @@ class Cardapio:
         'feijoada':tk.PhotoImage(file='assets\\feijoada.png'),
         }
 
+        #Content Backgroudn color: #edb3b0
+        #NavBar Backgroudn color: #f2c6c4
+        #Text Color: #626262
+
         #Header config
         self._header = addFrame(self.root, {'background':'#f2c6c4'}, { 'fill':'x', 'anchor':'n'})
         self._homeB = addLabel(self._header, {'image':self._assets['house'], 'borderwidth':0}, {'padx':(150,0), 'side':'left'})
@@ -85,8 +89,57 @@ class Cardapio:
         self._homeB.pack(padx=(150,0), side='left')
         self._banner.pack(fill='x', side='left', anchor='n', expand=True)
         self._cartB.pack(padx=(0,150), side='right', anchor='e')
-        deleteChildren(self.content)
-        print('yyoooooooooooooooooooooooooooooooo')
+        forgetChildren(self.content)
+        addLabel(self.content, {'font':('Helvetica', 24), 'text':f'Olá, {self.user} e abaixo as opções do restaurante, as opções possíveis são:', 'background':'#edb3b0', 'fg':'#626262'},
+                  {'fill':'x', 'padx':150, 'anchor':'center'})
+        holderHome = addFrame(self.content, {'background':'#F9B97D', 'width':1267, 'height':618}, {'anchor':'n', 'pady':20})
+        holderHome.columnconfigure(0, weight=1)
+        holderHome.columnconfigure(1, weight=1)
+        holderHome.columnconfigure(2, weight=1)
+        holderHome.rowconfigure(0, weight=1)
+        holderHome.rowconfigure(1, weight=1)
+
+        linha1 = addFrame(holderHome, {'background':'#F9B97D'}, {'fill':'x', 'pady':(10,0)})
+        linha1.columnconfigure(0, weight=1)
+        linha1.columnconfigure(1, weight=1)
+        linha1.columnconfigure(2, weight=1)
+
+        self._mg1 = tk.Label(linha1, image=self._assets['entrada'], borderwidth=0)
+        self._mg1.grid(column=0, row=0, padx=(10,0))
+        self._mg1.bind("<Button-1>", lambda e: print('yo1'))
+
+        tk.Label(linha1, text='Entrada', font=('Helvetica', 18), fg='#626262' ).grid(column=0, row=1, padx=(10,0), sticky='we')
+
+        self._mg2 = tk.Label(linha1, image=self._assets['pp'], borderwidth=0)
+        self._mg2.grid(column=1, row=0, padx=40)
+        self._mg2.bind("<Button-1>", lambda e: print('yo2'))
+        tk.Label(linha1, text='Prato Principal', font=('Helvetica', 18), fg='#626262' ).grid(column=1, row=1, padx=40, sticky='we')
+
+        self._mg3 = tk.Label(linha1, image=self._assets['bebidas'], borderwidth=0)
+        self._mg3.grid(column=2, row=0, padx=(0,10))
+        self._mg3.bind("<Button-1>", lambda e: print('yo3'))
+        tk.Label(linha1, text='Bebidas', font=('Helvetica', 18), fg='#626262' ).grid(column=2, row=1, padx=(0,10), sticky='we')
+
+        linha2 = addFrame(holderHome, {'background':'#F9B97D'}, {'fill':'x', 'pady':(20,10)})
+        linha2.columnconfigure(0, weight=1)
+        linha2.columnconfigure(1, weight=1)
+        linha2.columnconfigure(2, weight=1)
+
+        self._mg4 = tk.Label(linha2, image=self._assets['alcool'], borderwidth=0, text='text' )
+        self._mg4.grid(column=0, row=0, padx=(10,0))
+        self._mg4.bind("<Button-1>", lambda e: print('yo4'))
+        tk.Label(linha2, text='Bebidas Alcoólicas', font=('Helvetica', 18), fg='#626262' ).grid(column=0, row=1, padx=(10,0), sticky='we')
+
+        self._mg5 = tk.Label(linha2, image=self._assets['sobremesa'], borderwidth=0)
+        self._mg5.grid(column=1, row=0, padx=40)
+        self._mg5.bind("<Button-1>", lambda e: print('yo5'))
+        tk.Label(linha2, text='Sobremesas', font=('Helvetica', 18), fg='#626262' ).grid(column=1, row=1, padx=40, sticky='we')
+
+        self._mg6 = tk.Label(linha2, image=self._assets['chef'], borderwidth=0)
+        self._mg6.grid(column=2, row=0, padx=(0,10))
+        self._mg6.bind("<Button-1>", lambda e: print('yo6'))
+        tk.Label(linha2, text='Menu do Chef', font=('Helvetica', 18), fg='#626262' ).grid(column=2, row=1, padx=(0,10), sticky='we')
+
 
     def login(self):
         #Left Image
@@ -107,22 +160,24 @@ class Cardapio:
         #tk.Entry(borderwidth=)
 
 def validateCad(object: Cardapio, uEntry:tk.Entry, pEntry:tk.Entry, cpEntry:tk.Entry):
+    object.user = uEntry.get()
     user = uEntry.get()
     password = pEntry.get()
     cpassword = cpEntry.get()
-    #print(user, password, cpassword)
-    if(user == '' or len(user) < 5):
-        messagebox.showerror('Login Incorreto!', 'O login é muito curto!')
-        return
-    elif(password == '' or len(password) < 6):
-        messagebox.showerror('Senha Invalida!', 'A senha é muito curta!')
-        return
-    elif(user == password):
-        messagebox.showwarning('Senha Invalida!', 'A senha não pode ser igual ao login!')
-        return
-    elif(password != cpassword):
-        messagebox.showerror('Senha Incorreta!', 'As senhas são diferentes!')
-        return
+
+    #if(user == '' or len(user) < 5):
+    #    messagebox.showerror('Login Incorreto!', 'O login é muito curto!')
+    #    return
+    #elif(password == '' or len(password) < 6):
+    #    messagebox.showerror('Senha Invalida!', 'A senha é muito curta!')
+    #    return
+    #elif(user == password):
+    #    messagebox.showwarning('Senha Invalida!', 'A senha não pode ser igual ao login!')
+    #    return
+    #elif(password != cpassword):
+    #    messagebox.showerror('Senha Incorreta!', 'As senhas são diferentes!')
+    #    return
+
     uEntry.delete(0, 'end')
     pEntry.delete(0, 'end')
     cpEntry.delete(0, 'end')
