49e4a087e36f1db1cd5fc346a4a450ed4a299717
LuisCunha05 <luisf.cunha05@gmail.com>
2024-09-05 11:11:22 -0400

    Changed scope of calculator app idk
    app is now  an exerc. :+1:

--- apps/calc.py
+++ apps/calc.py
@@ -10,61 +10,60 @@ def getLastNumber(string: str) -> str:
             break
     return result[::-1]
 
+def setGeometry(master:tk.Tk, width:int = None, height:int = None,x:int = None, y:int = None, resizable:bool = True):
+    """Sets the window size and put it at center of the screen.
+    If WIDTH or HEIGHT were given than uses this values instead."""
+    x = x if x else int((master.winfo_screenwidth() - width) / 2)
+    y = y if y else int((master.winfo_screenheight() - height) / 2)
+
+    master.geometry(f'{width}x{height}+{x}+{y}')
+    master.resizable(resizable, resizable)
+
 class Pyculator:
     __symbols = ['+', '-', '/', '*', '.']
     MAX_INT = 9223372036854775807
     def __init__(self) -> None:
-        self.calc: str = '0'
-        self._parent_open = 0
-        self._parent_close = 0
+        self.expression: str = ''
+        self.number: str = '0'
+        self._parent_open: int = 0
+        self._parent_close: int = 0
     
     def addChar(self, char: str) -> str:
         """Adds a character to the Calculation string. CANNOT add a new symbol if the last character is a symbol"""
 
-        lastChar = self.getLastChar()
-        lastNum = getLastNumber(self.calc)
-        print(lastNum)
-        if(char.isnumeric()):
-            if(lastChar == ')'):
-                char += '*'
-            elif(lastNum != '' and float(lastNum) > self.MAX_INT):
-                return self.calc
-
-        match(char):
-            case '(':
-                if(self._parent_open >= 5):
-                    return self.calc
-                else:
-                    self._parent_open += 1
-                    temp = lastNum
-                    if(lastChar == ')' or temp != '0' and temp != '' ):
-                        char = '*' + char
-            case ')':
-                if(self._parent_close >= self._parent_open or lastNum == ''):
-                    return self.calc
-                else:
-                    self._parent_close += 1
-            case '.':
-                if(not lastChar.isnumeric() or '.' in lastNum):
-                    return self.calc
-
-        if(char in Pyculator.__symbols and lastChar in Pyculator.__symbols ):
-            return self.calc
+        if(char.isnumeric() or char == '.'):
+            self.addChar(char)
         
-        if(lastNum == '0' and char != ''):
-            self.calc = char
-            return self.calc
-        self.calc += char
-        return self.calc
+
+        #lastChar = self.getLastChar()
+        #lastNum = getLastNumber(self.expression)
+        
+    def addNumber(self, char:str) -> str:
+        
+        if(not self.getLastChar().isnumeric()):
+            self.number = char
+            return char
+        if(float(self.number) > self.MAX_INT):
+            char = ''
+        self.number += char
+        return self.number
     
     def clear(self) -> str:
-        self.calc = '0'
-        self._parent_open = 0
-        self._parent_close = 0
-        return self.calc
+        self.number = '0'
+        return self.number
+    
+    def getNumber(self) -> str:
+        return self.number
+    
+    def setNumber(self, num: str) -> str:
+        self.number = num
+        return self.number
     
     def getLastChar(self) -> str:
-        return self.calc[-1]
+        return self.expression[-1]
+
+    def getLastDigit(self) -> str:
+        return self.number[-1]
 
     def backSpace(self) -> str:
         match(self.getLastChar()):
@@ -73,106 +72,84 @@ class Pyculator:
             case ')':
                 self._parent_close -= 1
 
-        self.calc = '0' if self.calc[:-1] == '' else self.calc[:-1]
-        return self.calc
+        self.expression = '0' if self.expression[:-1] == '' else self.expression[:-1]
+        return self.expression
 
 
 
 class PyculatorGUI(Pyculator):
+    __FontS = ('Arial', 16)
+    __BgColor = '#4d4d4d'
+
     def __init__(self) -> None:
         super().__init__()
 
         self.root = tk.Tk()
-        self.root.geometry('500x600')
-        self.root.resizable(width=False, height=False)
+        setGeometry(self.root, 400, 500,None,None, False)
         self.root.title('Pyculator')
         self.root.configure(background='#282828')
 
-        self._stringCalc = tk.StringVar()
-        self._stringCalc.set(self.calc)
-        
-        self.pyc_label = tk.Label(
+        self._stringExp = tk.StringVar()
+        self._stringExp.set(self.expression)
+        self._stringNumber = tk.StringVar()
+        self._stringNumber.set(self.number)
+
+        #Label Expression
+        tk.Label(
             self.root,
-            textvariable=self._stringCalc,
+            textvariable=self._stringExp,
             font=('Arial', 14),
-            height=4,
+            height=3,
             highlightthickness=1,
             highlightbackground='gray',
             background='#282828',
-            fg='#ffffff',
+            fg='white',
             justify='right',
-            wraplength=468,
+            wraplength=368,
             anchor='se'
-            )
-        self.pyc_label.pack(padx=15,pady=15, fill='x')
+        ).pack(padx=15,pady=(15,1), fill='x')
+        #Label Number
+        tk.Label(
+            self.root,
+            textvariable=self._stringNumber,
+            font=('Arial', 14),
+            height=1,
+            highlightthickness=1,
+            highlightbackground='gray',
+            background='#282828',
+            fg='white',
+            justify='right',
+            wraplength=368,
+            anchor='se'
+        ).pack(padx=15,pady=(1,15), fill='x')
 
         #Numeric Frame
         self.btn_frame = tk.Frame(self.root)
-        self.btn_frame.columnconfigure(0, weight=1)
-        self.btn_frame.columnconfigure(1, weight=1)
-        self.btn_frame.columnconfigure(2, weight=1)
-
-        #Numbers
-        self.btn_1 = tk.Button(self.btn_frame, text='1', command= lambda: self._stringCalc.set(self.addChar('1')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_1.grid(row=2, column=0, sticky='we')
-        self.btn_2 = tk.Button(self.btn_frame, text='2', command= lambda: self._stringCalc.set(self.addChar('2')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_2.grid(row=2, column=1, sticky='we')
-        self.btn_3 = tk.Button(self.btn_frame, text='3', command= lambda: self._stringCalc.set(self.addChar('3')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_3.grid(row=2, column=2, sticky='we')
-        self.btn_4 = tk.Button(self.btn_frame, text='4', command= lambda: self._stringCalc.set(self.addChar('4')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_4.grid(row=1, column=0, sticky='we')
-        self.btn_5 = tk.Button(self.btn_frame, text='5', command= lambda: self._stringCalc.set(self.addChar('5')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_5.grid(row=1, column=1, sticky='we')
-        self.btn_6 = tk.Button(self.btn_frame, text='6', command= lambda: self._stringCalc.set(self.addChar('6')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_6.grid(row=1, column=2, sticky='we')
-        self.btn_7 = tk.Button(self.btn_frame, text='7', command= lambda: self._stringCalc.set(self.addChar('7')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_7.grid(row=0, column=0, sticky='we')
-        self.btn_8 = tk.Button(self.btn_frame, text='8', command= lambda: self._stringCalc.set(self.addChar('8')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_8.grid(row=0, column=1, sticky='we')
-        self.btn_9 = tk.Button(self.btn_frame, text='9', command= lambda: self._stringCalc.set(self.addChar('9')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_9.grid(row=0, column=2, sticky='we')
-        self.btn_0 = tk.Button(self.btn_frame, text='0', command= lambda: self._stringCalc.set(self.addChar('0')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_0.grid(row=3, column=1, sticky='we')
-        self.btn_pi = tk.Button(self.btn_frame, text='π', command= lambda: self._stringCalc.set(self.addChar('π')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_pi.grid(row=3, column=0, sticky='we')
-        self.btn_dot = tk.Button(self.btn_frame, text='.', command= lambda: self._stringCalc.set(self.addChar('.')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_dot.grid(row=3, column=2, sticky='we')
-
+        for i in range(4):
+            self.btn_frame.columnconfigure(i, weight=1)
         self.btn_frame.pack(padx=15, pady=15, fill='x')
 
-        #Operations Frame
-        self.op_frame = tk.Frame(self.root)
-        self.op_frame.columnconfigure(0, weight=1)
-        self.op_frame.columnconfigure(1, weight=1)
-        self.op_frame.columnconfigure(2, weight=1)
+        #Numbers
+        botoes_num = [
+            ('C', 0, 0), ('CE', 0, 1), ('🡐', 0, 2),('(', 0, 3),
+            (')', 1, 0), ('x^y', 1, 1), ('√', 1, 2),('+', 1, 3),
+            ('7', 2, 0), ('8', 2, 1), ('9', 2, 2),('-', 2, 3),
+            ('4', 3, 0), ('5', 3, 1), ('6', 3, 2),('🞶', 3, 3),
+            ('1', 4, 0), ('2', 4, 1), ('3', 4, 2),('/', 4, 3),
+            ('+/-', 5, 0), ('0', 5, 1), ('.', 5, 2),('=', 5, 3),
+        ]
 
-        #Commands
-        self.btn_clear = tk.Button(self.op_frame, text='C', command= lambda: self._stringCalc.set(self.clear()), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_clear.grid(row=0, column=0, sticky='we')
-        self.btn_clear_entry = tk.Button(self.op_frame, text='CE', command= lambda: self._stringCalc.set(self.clear()), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_clear_entry.grid(row=0, column=1, sticky='we')
-        self.btn_backspace = tk.Button(self.op_frame, text='🡐', command= lambda: self._stringCalc.set(self.backSpace()), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_backspace.grid(row=0, column=2, sticky='we')
-
-        #Operations
-        self.btn_plus = tk.Button(self.op_frame, text='+', command= lambda: self._stringCalc.set(self.addChar('+')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_plus.grid(row=1, column=0, sticky='we')
-        self.btn_minus = tk.Button(self.op_frame, text='-', command= lambda: self._stringCalc.set(self.addChar('-')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_minus.grid(row=1, column=1, sticky='we')
-        self.btn_multiply = tk.Button(self.op_frame, text='🞶', command= lambda: self._stringCalc.set(self.addChar('*')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_multiply.grid(row=1, column=2, sticky='we')
-        self.btn_division = tk.Button(self.op_frame, text='/', command= lambda: self._stringCalc.set(self.addChar('/')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_division.grid(row=2, column=0, sticky='we')
-        self.btn_open_parent = tk.Button(self.op_frame, text='(', command= lambda: self._stringCalc.set(self.addChar('(')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_open_parent.grid(row=2, column=1, sticky='we')
-        self.btn_close_parent = tk.Button(self.op_frame, text=')', command= lambda: self._stringCalc.set(self.addChar(')')), font=('Arial', 16), background='#4d4d4d', fg='#ffffff')
-        self.btn_close_parent.grid(row=2, column=2, sticky='we')
-
-        self.op_frame.pack(padx=15, fill='x')
-
-        #self.btn_quit = tk.Button(self.root, text="Quit", font=('Arial', 16), background='#4d4d4d', fg='#ffffff', width=15, command=self.root.destroy).pack()
+        for char, row, col in botoes_num:
+            tk.Button(
+                self.btn_frame,
+                text=char,
+                command= lambda x = char: self._stringNumber.set(self.addChar(x)),
+                font=self.__FontS,
+                background=self.__BgColor if char != '=' else '#007fff',
+                fg='white'
+            ).grid(row=row, column=col, sticky='we')
 
         self.root.mainloop()
 
-
-PyculatorGUI()
+if __name__ == '__main__':
+    PyculatorGUI()
