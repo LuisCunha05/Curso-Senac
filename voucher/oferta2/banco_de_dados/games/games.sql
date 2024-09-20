bb916fd39c529d7a795a5edf6081273d190f7f20
LuisCunha05 <luisf.cunha05@gmail.com>
2024-09-20 09:24:58 -0300

    Update games.sql

--- voucher/oferta2/banco_de_dados/games/games.sql
+++ voucher/oferta2/banco_de_dados/games/games.sql
@@ -1,4 +1,16 @@
 create database game;
 use game;
 
-select * from vendas; 
+describe venda;
+select * from venda;
+select count(*) from venda;
+select avg(Global_Sales) from venda;
+select * from venda where Global_Sales > (select avg(Global_Sales) from venda);
+select * from venda where Global_Sales > 2;
+select * from venda where Platform='PS2' and Global_Sales > 10;
+select * from venda where Platform='Wii' order by JP_Sales desc;
+select * from venda where Platform='Wii' order by NA_Sales desc;
+select * from venda where Name like 'Just Dance%' order by Global_Sales desc;
+select * from venda where Publisher='Ubisoft' order by Global_Sales desc;
+select * from venda where Publisher like 'Sony%' order by Global_Sales desc;
+select distinct Publisher from venda;
