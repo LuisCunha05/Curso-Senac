88555f74ceb0c59383792fee83770f0d91d04395
LuisCunha05 <luisf.cunha05@gmail.com>
2025-03-12 08:47:09 -0400

    alterado como flutter carrega apps

--- voucher/oferta2/mobile/ativ1/atividade_1/test/test_name_changer.dart
+++ voucher/oferta2/mobile/ativ1/atividade_1/test/test_name_changer.dart
@@ -5,26 +5,23 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:atividade_1/main.dart';
+import 'package:atividade_1/aula01/name_changer.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NameChangerApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Mudar Nome'), findsOneWidget);
+    expect(find.text('Elaias'), findsOne);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('Mudar Nome'));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Elias'), findsNothing);
   });
 }
