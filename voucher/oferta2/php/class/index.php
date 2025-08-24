c4e07885c482cf118c739fe63ff828ccc0ee78cd
LuisCunha05 <luisf.cunha05@gmail.com>
2025-08-24 03:53:40 -0300

    :sparkles: feat: ufms

--- voucher/oferta2/php/class/index.php
+++ voucher/oferta2/php/class/index.php
@@ -1,24 +1,47 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-class Goodbye {
-  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
-  public $name;
 
-  function __construct($name){
-  	$this->name = $name;
+function br(){
+  echo "<br><br>";
+}
+
+class Pessoa {
+  private $nome;
+
+  public function setNome($nome) {
+      $this->nome = $nome;
   }
-  public function byebye() {
-    echo self::LEAVING_MESSAGE;
-    echo $this->name;
+
+  public function getNome() {
+      return $this->nome;
   }
 }
 
-$goodbye = new Goodbye("opa!");
-$goodbye->byebye();
+class Funcionario extends Pessoa {
+  protected $cargo;
+
+  public function setCargo($cargo) {
+      $this->cargo = $cargo;
+  }
+
+  public function getCargo() {
+      return $this->cargo;
+  }
+
+  // public function trabalha(){
+  //   echo "$this->nome é trabalhador";
+  // }
+
+  // public function setNome(){
+  //   $this->nome = 'opa';
+  // }
+}
+
+$funcionario = new Funcionario();
+$funcionario->setNome("Carlos");
+$funcionario->setCargo("Desenvolvedor");
+
+echo "Nome: " . $funcionario->getNome() . "<br>";
+echo "Cargo: " . $funcionario->getCargo() . "<br>";
+
 ?>
 
-</body>
-</html>
